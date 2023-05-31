275d1446e315cad4162193d7ebdc5ae56f78c2d2
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-31 14:18:15 +0900

    feat: POSTリクエスト時のリクエストボディの取得

--- golang/src/main.go
+++ golang/src/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	_ "encoding/json"
+	_ "fmt"
+	_ "io"
+	"net/http"
 	"todo-go/controllers"
 
 	_ "context"
@@ -15,6 +19,18 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		responce := controllers.Read()
 		c.JSON(200, responce)
+	})
+	r.POST("/create", func(c *gin.Context) {
+		var todo_create struct {
+			Task        string `json:"task"`
+			Description string `json:"description"`
+		}
+		if err := c.ShouldBindJSON(&todo_create); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"task": todo_create.Task, "description": todo_create.Description})
+
 	})
 	r.Run()
 }
